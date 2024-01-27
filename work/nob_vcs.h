1def038bcd5f4388c6b17c29d84f9d055fada2fa
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2024-01-27 16:11:05 +0800

    Update nob_vcs.h

--- work/nob_vcs.h
+++ work/nob_vcs.h
@@ -1,6 +1,5 @@
 #define nob_cmd_append_vcs(cmd) \
 	nob_cmd_append(&cmd, "vcs", "-o", "./simv"); \
-	nob_cmd_append(&cmd, "-cc", "clang", "-cpp", "clang++"); \
 	nob_cmd_append(&cmd, "-j16", "-RI", "-full64"); \
 	nob_cmd_append(&cmd, "-sverilog", "+v2k"); \
 	nob_cmd_append(&cmd, "-debug_access+all", "-kdb", "+sdfverbos"); \
