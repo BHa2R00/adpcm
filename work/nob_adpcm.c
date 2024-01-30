1a8b9192205ab2f0c1edb97775723878d93d7425
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2024-01-30 19:24:26 +0800

    Update nob_adpcm.c

--- work/nob_adpcm.c
+++ work/nob_adpcm.c
@@ -11,16 +11,41 @@ Nob_Proc build_c(){
 	return nob_cmd_run_sync(cmd);
 }
 
-Nob_Proc run_c(){
+Nob_Proc run_print_verilog(){
 	Nob_Cmd cmd = {0};
-	nob_cmd_append(&cmd, "../bin/adpcm");
+	nob_cmd_append(&cmd, "../bin/adpcm", "print_verilog");
 	return nob_cmd_run_sync(cmd);
 }
 
-Nob_Proc build_tb(){
+Nob_Proc run_c_test1(){
+	Nob_Cmd cmd = {0};
+	nob_cmd_append(&cmd, "../bin/adpcm", "test1");
+	return nob_cmd_run_sync(cmd);
+}
+
+Nob_Proc run_c_test2_stage1(){
+	Nob_Cmd cmd = {0};
+	nob_cmd_append(&cmd, "../bin/adpcm", "test2_stage1");
+	return nob_cmd_run_sync(cmd);
+}
+
+Nob_Proc run_c_test2_stage4(){
+	Nob_Cmd cmd = {0};
+	nob_cmd_append(&cmd, "../bin/adpcm", "test2_stage4");
+	return nob_cmd_run_sync(cmd);
+}
+
+Nob_Proc build_tb_test1(){
 	Nob_Cmd cmd = {0};
 	nob_cmd_append_vcs(cmd);
-	nob_cmd_append(&cmd, "../tb/adpcm_tb.sv");
+	nob_cmd_append(&cmd, "+define+ENABLE_TRACE", "+define+ENABLE_TEST1", "../tb/adpcm_tb.sv");
+	return nob_cmd_run_sync(cmd);
+}
+
+Nob_Proc build_tb_test2(){
+	Nob_Cmd cmd = {0};
+	nob_cmd_append_vcs(cmd);
+	nob_cmd_append(&cmd, "+define+ENABLE_TEST2", "../tb/adpcm_tb.sv");
 	return nob_cmd_run_sync(cmd);
 }
 
@@ -34,8 +59,13 @@ int main(int argc, char** argv){
 	NOB_GO_REBUILD_URSELF(argc, argv);
 	Nob_Procs procs = {0};
 	nob_da_append(&procs, build_c());
-	nob_da_append(&procs, run_c());
-	nob_da_append(&procs, build_tb());
+	nob_da_append(&procs, run_print_verilog());
+	nob_da_append(&procs, run_c_test1());
+	nob_da_append(&procs, build_tb_test1());
 	nob_da_append(&procs, run_tb());
+	nob_da_append(&procs, run_c_test2_stage1());
+	nob_da_append(&procs, build_tb_test2());
+	nob_da_append(&procs, run_tb());
+	nob_da_append(&procs, run_c_test2_stage4());
 	return 0;
 }
